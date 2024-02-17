c6bf2da91e18ac54ef4d51fdf44c6824b046d113
DARKnx <79696304+DARKnx@users.noreply.github.com>
2024-02-17 03:22:09 +0000

    :construction: Create category router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import usersRouter from "./resources/users.router.js";
+import categorysRouter from "./resources/categorys.router.js";
 
 export const router = Router();
 
@@ -8,6 +9,7 @@ router.get("/ping", (req, res) => {
   res.sendStatus(200);
 });
 
+router.use('/category', categorysRouter);
 router.use('/user', usersRouter);
 
 
---
+++ src/routes/resources/categorys.router.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+import categoryController from "../../resources/category/category.controllers.js";
+import auth from '../../middlewares/auth.js'
+
+const service = new categoryController();
+const categoryRouter = Router();
+
+categoryRouter.delete("/delete", auth, service.delete);
+categoryRouter.post("/create", auth, service.create);
+categoryRouter.put("/update", auth, service.update);
+categoryRouter.get("/get", auth, service.get);
+
+
+export default categoryRouter;
