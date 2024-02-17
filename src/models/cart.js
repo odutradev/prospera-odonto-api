de5aa07f7014a40ffc2f3910caffbfdeb29f892a
DARKnx <79696304+DARKnx@users.noreply.github.com>
2024-02-17 02:54:17 +0000

    :construction: Create cart model

---
+++ src/models/cart.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const CartSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+	description:{
+		type: String,
+		default: ""
+	},
+	value:{
+		type: Number,
+		default: 0
+	},
+	valueRange:{
+		start:{
+			type:Number,
+			default: 0
+		},
+		end:{
+			type: Number,
+			default: 0
+		}
+	},
+	images:{
+		type: Array
+	},
+	links:{
+		type: Array
+	},
+	status:{
+		type: String,
+		default: "não comprado"
+	},
+	category:{
+		type: String,
+	},
+	priority:{
+		type: Number
+	}
+});
+
+export default mongoose.model('cart', CartSchema);
