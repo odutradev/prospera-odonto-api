6ae3a833aa6cff00c8adc22a15f4c21b0b76649e
DARKnx <79696304+DARKnx@users.noreply.github.com>
2024-02-17 02:56:59 +0000

    :construction: Create category model

---
+++ src/models/category.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const CategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+	description:{
+		type: String,
+		default: ""
+	},
+	date:{
+		type: Date,
+		default: Date.now()
+	}
+});
+
+export default mongoose.model('category', CategorySchema);
