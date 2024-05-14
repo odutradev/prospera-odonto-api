ab0df575bb1bdb2ed67327897777d511ee04ea05
dutra-pc <joao.vitornl@gmail.com>
2024-05-14 11:11:57 -0300

    :construction:  Create service router

---
+++ src/routes/resources/service.router.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+import serviceController from "../../resources/service/service.controllers.js";
+import auth from '../../middlewares/auth.js'
+
+const service = new serviceController();
+const serviceRouter = Router();
+
+serviceRouter.delete("/delete", auth, service.delete);
+serviceRouter.post("/create", auth, service.create);
+serviceRouter.put("/update", auth, service.update);
+serviceRouter.get("/get", auth, service.get);
+
+export default serviceRouter;
