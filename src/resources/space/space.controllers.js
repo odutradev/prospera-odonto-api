cdbd1a9161745e7f20244f7a0a288adeb18b119c
dutra-pc <joao.vitornl@gmail.com>
2024-05-23 17:42:17 -0300

    :bricks: Create getSpacesAndServices function and route

--- src/resources/space/space.controllers.js
+++ src/resources/space/space.controllers.js
@@ -4,6 +4,7 @@ import Service from "./space.service.js";
 export default class Controller {
 	service = new Service();
 	
+	getSpacesAndServices = async (req, res) => handleRequest(req, res, this.service.getSpacesAndServices);
 	create = async (req, res) => handleRequest(req, res, this.service.create);
 	update = async (req, res) => handleRequest(req, res, this.service.update);
 	delete = async (req, res) => handleRequest(req, res, this.service.delete);
--- src/resources/space/space.service.js
+++ src/resources/space/space.service.js
@@ -1,3 +1,4 @@
+import serviceModel from "../../models/service.js";
 import spaceModel from "../../models/space.js";
 
 export default class Service {
@@ -5,6 +6,19 @@ export default class Service {
     async get({}, author){
         try {
 			return await spaceModel.find({author}).sort({ date: -1 });
+        } catch (err) {
+            return { error: "internal_error" } ;
+        }
+    }
+    async getSpacesAndServices({author}){
+        try {
+			var spaces = await spaceModel.find({author}).sort({ date: -1 });
+            return  await Promise.all(
+                spaces.map(async (space) => {
+                  var data = await serviceModel.find({ space: space._id }).sort({ date: -1 });
+                  return { name: space.name, spaceID: space._id, data };
+                })
+              );
         } catch (err) {
             return { error: "internal_error" } ;
         }
