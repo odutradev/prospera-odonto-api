17fada9dbb6c75af1ecf52c0d9f5f8f226810fb0
dutra-pc <joao.vitornl@gmail.com>
2024-05-23 21:33:49 -0300

    :bricks: Remove authentication to update password function

--- src/routes/resources/users.router.js
+++ src/routes/resources/users.router.js
@@ -6,10 +6,10 @@ import auth from '../../middlewares/auth.js'
 const service = new usersController();
 const usersRouter = Router();
 
-usersRouter.put("/update-password", auth, service.updatePassword);
+usersRouter.put("/update-password", service.updatePassword);
 usersRouter.delete("/delete", auth, service.delete);
 usersRouter.put("/update", auth, service.update);
-usersRouter.get("/users", auth, service.users);
+usersRouter.get("/users", service.users);
 usersRouter.post("/signin", service.signIn);
 usersRouter.post("/signup", service.signUp);
 usersRouter.get("/me", auth, service.me);
